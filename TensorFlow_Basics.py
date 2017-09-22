890b1f0ed985d7f45953bed318d61843e86dc1ae
Corrected Below <32191869+correctedbelow@users.noreply.github.com>
2017-09-22 13:31:31 +0300

    Placeholders

--- TensorFlow_Basics.py
+++ TensorFlow_Basics.py
@@ -8,10 +8,11 @@ print('Python {}'.format(sys.version))
 print('TensorFlow {}'.format(tf.__version__))
 
 
+x = tf.placeholder(tf.float32)
 W = tf.Variable([0.1], dtype=tf.float32) # A single weight
 b = tf.Variable([-0.3], dtype=tf.float32)
 
 session = tf.Session()
 
 session.run(tf.global_variables_initializer()) # REMEMBER: Always initialize your variables!
-print(session.run([W, b]))
+print(session.run(x, {x: 5.0}))
