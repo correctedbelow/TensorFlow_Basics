f917ea4187c4f8b71852d1b30360bb4b1571986d
Corrected Below <32191869+correctedbelow@users.noreply.github.com>
2017-09-22 13:36:35 +0300

    Two Layers

--- TensorFlow_Basics.py
+++ TensorFlow_Basics.py
@@ -9,9 +9,14 @@ print('TensorFlow {}'.format(tf.__version__))
 
 
 x = tf.placeholder(tf.float32)
-W = tf.Variable([[0.1], [0.2]], dtype=tf.float32)
-b = tf.Variable([-0.3], dtype=tf.float32)
-y = tf.matmul(x, W) + b
+W1 = tf.Variable([[0.1], [0.2]], dtype=tf.float32)
+b1 = tf.Variable([-0.3, 0.4], dtype=tf.float32)
+W2 = tf.Variable([[0.5], [-0.6]], dtype=tf.float32)
+b2 = tf.Variable([0.7], dtype=tf.float32)
+h = tf.matmul(x, W1) + b1
+h = tf.sigmoid(h)
+y = tf.matmul(h, W2) + b2
+
 session = tf.Session()
 
 session.run(tf.global_variables_initializer()) # REMEMBER: Always initialize your variables!
