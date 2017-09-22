c6e775ed1b8ec286aca0746319ae771bd757b309
Corrected Below <32191869+correctedbelow@users.noreply.github.com>
2017-09-22 13:38:15 +0300

    tf.layers

--- TensorFlow_Basics.py
+++ TensorFlow_Basics.py
@@ -8,15 +8,9 @@ print('Python {}'.format(sys.version))
 print('TensorFlow {}'.format(tf.__version__))
 
 
-x = tf.placeholder(tf.float32)
-W1 = tf.Variable([[0.1], [0.2]], dtype=tf.float32)
-b1 = tf.Variable([-0.3, 0.4], dtype=tf.float32)
-W2 = tf.Variable([[0.5], [-0.6]], dtype=tf.float32)
-b2 = tf.Variable([0.7], dtype=tf.float32)
-h = tf.matmul(x, W1) + b1
-h = tf.sigmoid(h)
-y = tf.matmul(h, W2) + b2
-
+x = tf.placeholder(tf.float32, shape=(None, 2))
+h = tf.layers.dense(x, units=2, activation=tf.sigmoid, use_bias=True)
+y = tf.layers.dense(h, units=1, use_bias=True)
 session = tf.Session()
 
 session.run(tf.global_variables_initializer()) # REMEMBER: Always initialize your variables!
